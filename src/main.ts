b9d684e70942493cde7afdd56a84f54b83fd1b95
40302258-LorenzoCueto <lcueto01@qub.ac.uk>
2024-01-22 10:38:41 +0000

    add label

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./app.css";
 import App from "./App.svelte";
 
-console.log("Hello 223323ff46655555");
+console.log("Hello 223323ff4665555555");
 
 const app = new App({
   target: document.getElementById("app"),
