52e7232ddaf54177cf92c19676a8ed2b8e2952bb
40302258-LorenzoCueto <lcueto01@qub.ac.uk>
2024-01-22 20:11:42 +0000

    jin

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./app.css";
 import App from "./App.svelte";
 
-console.log("Hello 223323ff4665555555");
+console.log("Hello 223323ff466555555566");
 
 const app = new App({
   target: document.getElementById("app"),
